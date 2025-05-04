d84791dd8c68fb3c2524a6e286292277d444a795
PowreSales <ezra.seidu74@gmail.com>
2025-05-04 17:26:50 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
 const CACHE_NAME = 'medicine-sales-v1';
-   const urlsToCache = [
-     '/',
-     '/index.html',
-     'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
-     'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js',
-     'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css',
-     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
-   ];
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
+  'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js',
+  'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+];
 
-   self.addEventListener('install', event => {
-     event.waitUntil(
-       caches.open(CACHE_NAME)
-         .then(cache => cache.addAll(urlsToCache))
-     );
-   });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
 
-   self.addEventListener('fetch', event => {
-     event.respondWith(
-       caches.match(event.request)
-         .then(response => response || fetch(event.request))
-     );
-   });
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
